a8d669f6f378887932296cc707a1c7a74e749b34
Julien ROUSSEAU <jrousseau@doubletrade.com>
2017-09-08 18:54:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+[![Build status](https://travis-ci.org/DoubleTrade/custom-scrollbar.svg?branch=master)](https://travis-ci.org/DoubleTrade/custom-scrollbar)
+[![Published on webcomponents.org](https://img.shields.io/badge/webcomponents.org-published-blue.svg)](https://www.webcomponents.org/element/doubletrade/range-datepicker)
+
+## &lt;custom-scrollbar&gt;
+
+`custom-scrollbar` provides a simple custom scrollbar for a container.
+
+### Install
+
+    bower install custom-scrollbar
+
+[See live demo](https://doubletrade.github.io/custom-scrollbar/components/custom-scrollbar/demo/)
+[See docs](https://doubletrade.github.io/custom-scrollbar/)
+
+<!---
+```
+<custom-element-demo>
+  <template>
+    <script src="../webcomponentsjs/webcomponents-lite.js"></script>
+    <link rel="import" href="custom-scrollbar">
+    <style is="custom-style">
+      div {
+        height: 320px;
+        width: 100%;
+      }
+    </style>
+    <next-code-block></next-code-block>
+  </template>
+</custom-element-demo>
+```
+-->
+```html
+<div>
+  <custom-scrollbar></custom-scrollbar>
+</div>
+```
---
+++ custom-scrollbar.js
@@ -0,0 +1,109 @@
+/**
+ * `custom-scrollbar`
+ * 
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+*/
+
+class CustomScrollbar extends Polymer.mixinBehaviors(
+  [Polymer.IronScrollTargetBehavior],
+  Polymer.Element
+) {
+  static get is() {
+    return 'custom-scrollbar';
+  }
+  static get properties() {
+    return {
+      _heightContent: Number,
+      _scrollbarHeight: Number,
+      _scrollbarTop: {
+        type: Number,
+        value: 0,
+      },
+      _dragging: Object,
+    };
+  }
+
+  _scrollHandler() {
+    if (this._dragging) return;
+    const visibleArea = this.scrollTarget.scrollHeight - this.scrollTarget.clientHeight;
+    const scrollbarPositionPercent = this._scrollTop / visibleArea;
+    this._scrollbarTop = scrollbarPositionPercent * (this._heightContent - this._scrollbarHeight);
+    this.shadowRoot.querySelector('.scrollbar-bar').style.transform = `translateY(${this
+      ._scrollbarTop}px)`;
+  }
+
+  ready() {
+    super.ready();
+
+    this.scrollTarget = this.shadowRoot.querySelector('.scrollbar-hidden');
+
+    this._heightContent = this.shadowRoot.querySelector('.scrollbar').offsetHeight;
+    const scrollbarChild = this.shadowRoot.querySelector('.scrollbar-child');
+    const visibleAreaPercent = this._heightContent * 100 / scrollbarChild.offsetHeight;
+    const scrollbarHeight = this._heightContent * visibleAreaPercent / 100;
+    this._scrollbarHeight = scrollbarHeight > 20 ? scrollbarHeight : 20;
+    this.shadowRoot.querySelector('.scrollbar-bar').style.height = `${this._scrollbarHeight}px`;
+
+    const scrollbarBar = this.shadowRoot.querySelector('.scrollbar-bar');
+
+    this.draggable(scrollbarBar);
+  }
+
+  draggable(element) {
+    this._dragging = null;
+
+    element.addEventListener('mousedown', (event) => {
+      this._dragging = {
+        mouseY: event.clientY,
+        startY: parseInt(!this._scrollbarTop ? 0 : this._scrollbarTop),
+      };
+      if (element.setCapture) element.setCapture();
+    });
+
+    element.addEventListener('losecapture', () => {
+      this._dragging = null;
+    });
+
+    document.addEventListener(
+      'mouseup',
+      () => {
+        this._dragging = null;
+      },
+      true
+    );
+
+    const dragTarget = element.setCapture ? element : document;
+
+    dragTarget.addEventListener(
+      'mousemove',
+      (event) => {
+        if (this._dragging) {
+          event.preventDefault();
+        } else {
+          return;
+        }
+
+        const scrollbarChild = this.shadowRoot.querySelector('.scrollbar-child');
+
+        this._scrollbarTop = this._dragging.startY + (event.clientY - this._dragging.mouseY);
+
+        const scrollTop =
+          this._scrollbarTop /
+          (this._heightContent - this._scrollbarHeight) *
+          (this.scrollTarget.scrollHeight - this.scrollTarget.clientHeight);
+
+        if (scrollTop + this._heightContent >= scrollbarChild.offsetHeight) {
+          return;
+        }
+        element.style.transform = `translateY(${Math.max(0, this._scrollbarTop)}px)`;
+        this.scrollTarget.scrollTop = scrollTop;
+      },
+      true
+    );
+  }
+}
+
+window.customElements.define(CustomScrollbar.is, CustomScrollbar);
